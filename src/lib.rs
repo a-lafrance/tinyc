508eb9a1298dc3c91569a64bb3fe11e53b31bc0c
Arthur Lafrance <lafrancearthur@gmail.com>
2022-04-04 13:28:20 -0700

    move driver to lib directly

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,37 @@
 #![feature(iterator_try_collect)]
 
-pub mod driver;
-
 pub(crate) mod ast;
 pub(crate) mod parser;
 pub(crate) mod scanner;
+
+use std::{ffi::OsString, fs::File, io::Read};
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[clap(author, version, about)]
+struct Config {
+    #[clap(help = "The source file to compile")]
+    input: String,
+
+    #[clap(short, long, default_value = "out", help = "The output file path")]
+    output: String,
+}
+
+pub fn start<Args, T>(args: Args)
+where
+    Args: IntoIterator<Item = T>,
+    T: Into<OsString> + Clone,
+{
+    let config = Config::parse_from(args);
+    let mut src_file = File::open(config.input).expect("failed to open input file");
+
+    let mut input = String::new();
+    src_file
+        .read_to_string(&mut input)
+        .expect("failed to read source file");
+
+    match scanner::tokenize(&input).try_collect::<Vec<_>>() {
+        Ok(tokens) => println!("{:?}", tokens),
+        Err(e) => eprintln!("{}", e),
+    };
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::env;
-use tinyc::driver;
 
 fn main() {
-    driver::start(env::args());
+    tinyc::start(env::args());
 }
