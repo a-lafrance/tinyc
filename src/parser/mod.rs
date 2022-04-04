5aa3e237304d6907716c71dd8368c879bb64b0ed
Arthur Lafrance <lafrancearthur@gmail.com>
2022-04-04 11:56:08 -0700

    parse trait setup

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -3,6 +3,18 @@ use crate::{
     scanner::TokenResult,
 };
 
-pub fn parse(tokens: impl Iterator<Item = TokenResult>) -> Computation {
-    todo!()
+type ParseResult<T> = Result<T, ()>;
+
+pub trait Parse: Sized {
+    fn parse(tokens: impl Iterator<Item = TokenResult>) -> ParseResult<Self>;
+}
+
+pub fn parse(tokens: impl Iterator<Item = TokenResult>) -> ParseResult<Computation> {
+    Computation::parse(tokens)
+}
+
+impl Parse for Computation {
+    fn parse(tokens: impl Iterator<Item = TokenResult>) -> ParseResult<Self> {
+        todo!()
+    }
 }
