8c8a8494ee0938d3e9780ac8a59c59916a8ec8c7
Arthur Lafrance <lafrancearthur@gmail.com>
2022-04-16 15:20:30 -0700

    tweak driver/fmt api

---
+++ src/driver/mod.rs
@@ -0,0 +1,62 @@
+mod dump;
+
+use std::{
+    ffi::OsString,
+    fs::File,
+    io::{self, Read},
+};
+use clap::Parser as ArgParse;
+use crate::{
+    ir::IrStore,
+    parser::Parser,
+    scanner,
+};
+use self::dump::{dump_ir, IrDumpFormat};
+
+#[derive(Debug, ArgParse)]
+#[clap(author, version, about)]
+struct Config {
+    #[clap(help = "The source file to compile")]
+    input: String,
+
+    #[clap(short, long, help = "The output file path")]
+    output: Option<String>,
+
+    #[clap(long, help = "Format to dump generated IR")]
+    dump_ir: Option<IrDumpFormat>,
+}
+
+pub fn start<Args, T>(args: Args)
+where
+    Args: IntoIterator<Item = T>,
+    T: Into<OsString> + Clone,
+{
+    let config = Config::parse_from(args);
+    let mut src_file = File::open(config.input).expect("failed to open input file");
+
+    let mut input = String::new();
+    src_file
+        .read_to_string(&mut input)
+        .expect("failed to read source file");
+
+    let tokens = scanner::tokenize(&input);
+
+    match Parser::new(tokens).and_then(|mut p| p.parse_computation()) {
+        Ok(ast) => {
+            let ir = IrStore::from(ast);
+
+            if let Some(dump_fmt) = config.dump_ir {
+                // FIXME: better error handling when opening outfile
+                let mut outfile = config.output.map(|f| File::create(f).expect("failed to open output file"));
+                let dump_result = match outfile.as_mut() {
+                    Some(outfile) => dump_ir(dump_fmt, outfile, &ir),
+                    None => dump_ir(dump_fmt, &mut io::stdout(), &ir),
+                };
+
+                dump_result.expect("failed to dump IR");
+            }
+        },
+
+        Err(e) => eprintln!("\x1b[31merror\x1b[0m: {}", e),
+    };
+}
