5ab5981a65a24e4c8f07ae0bfa2dedc1e11c15f3
Arthur Lafrance <lafrancearthur@gmail.com>
2022-04-25 19:07:34 -0700

    more ig construction sanity checks

--- src/regalloc/graph.rs
+++ src/regalloc/graph.rs
@@ -89,36 +89,6 @@ impl From<&Body> for InterferenceGraph {
         let mut live_set = HashSet::new();
 
         if let Some(root) = body.root_block() {
-            // ACTUALLY, don't need to make cfg bidirectional
-            // instead, need to make this routine recursive
-            // starting at root block, just visit its children first
-            // then, visit the root
-            // let's concoct an algorithm for this shall we
-
-            // situations:
-                // fallthrough ONLY:
-                    // visit fallthrough dest
-                    // use resulting live set
-                    // visit current block
-                // no children:
-                    // use empty live set
-                    // visit current block
-                // if statement:
-                    // visit then/else together
-                    // visit condition block
-                // then/else:
-                    // visit join block
-                    // split up live set
-                        // make 2 copies of live set
-                        // for each phi in join block:
-                            // exclude else operand in then side copy
-                            // exclude then operand in else side copy
-                    // visit then/else blocks separately with corresponding live set
-                        // IMPORTANT: stop visiting when you get to the join block
-                    // then union live sets together into master copy
-
-
-
             ig.visit_basic_block(body, root, &mut live_set, None);
         }
 
@@ -130,11 +100,176 @@ impl From<&Body> for InterferenceGraph {
 #[cfg(test)]
 mod tests {
     use maplit::{hashmap, hashset};
-    use crate::ir::isa::{Instruction, StoredBinaryOpcode};
+    use crate::ir::isa::{BranchOpcode, Instruction, StoredBinaryOpcode};
     use super::*;
 
     #[test]
-    fn interference_graph_single_bb_sanity_check() {
+    fn construct_ig_from_single_bb() {
+        /*
+            $0 = read
+            $1 = read
+            write $0
+
+            $2 = add $0, $1
+            write $2
+        */
+
+        let body = Body::from(
+            vec![BasicBlockData::with(
+                vec![
+                    Instruction::Read(Value(0)),
+                    Instruction::Read(Value(1)),
+                    Instruction::Write(Value(0)),
+                    Instruction::StoredBinaryOp {
+                        opcode: StoredBinaryOpcode::Add,
+                        src1: Value(0),
+                        src2: Value(1),
+                        dest: Value(2),
+                    },
+                    Instruction::Write(Value(2)),
+                    Instruction::End,
+                ],
+                None,
+                None,
+                None,
+            )],
+            Some(BasicBlock(0)),
+        );
+
+        let ig = InterferenceGraph::from(&body);
+        assert_eq!(ig, InterferenceGraph(hashmap! {
+            Value(0) => hashset! { Value(1) },
+            Value(1) => hashset! { Value(0) },
+            Value(2) => HashSet::new(),
+        }));
+    }
+
+    #[test]
+    fn construct_ig_from_fallthrough_only_blocks() {
+        /*
+            BB0:
+            $0 = const 0
+            $1 = const 1
+
+            BB1:
+            $2 = add $0, $1
+            write $2
+            end
+        */
+
+        let body = Body::from(
+            vec![BasicBlockData::with(
+                vec![
+                    Instruction::Const(0, Value(0)),
+                    Instruction::Const(1, Value(1)),
+                    Instruction::StoredBinaryOp {
+                        opcode: StoredBinaryOpcode::Add,
+                        src1: Value(0),
+                        src2: Value(1),
+                        dest: Value(2),
+                    },
+                    Instruction::Write(Value(2)),
+                ],
+                None,
+                None,
+                None,
+            )],
+            Some(BasicBlock(0)),
+        );
+
+        let ig = InterferenceGraph::from(&body);
+        assert_eq!(ig, InterferenceGraph(hashmap! {
+            Value(0) => hashset! { Value(1) },
+            Value(1) => hashset! { Value(0) },
+            Value(2) => HashSet::new(),
+        }));
+    }
+
+    #[test]
+    #[ignore]
+    fn construct_ig_from_if_stmt() {
+        // outputs the max of two input values
+        /*
+            BB0:
+            $0 = read
+            $1 = read
+            cmp $0, $1
+            ble BB2
+
+            BB1:
+            write $0
+            br BB3
+
+            BB2:
+            write $1
+
+            BB3:
+            $2 = add $0, $1
+            write $2
+            writeln
+            end
+        */
+
+        let body = Body::from(
+            vec![
+                BasicBlockData::with(
+                    vec![
+                        Instruction::Read(Value(0)),
+                        Instruction::Read(Value(1)),
+                        Instruction::Cmp(Value(0), Value(1)),
+                        Instruction::Branch(BranchOpcode::Ble, BasicBlock(2)),
+                    ],
+                    Some(BasicBlock(1)),
+                    Some(BasicBlock(2)),
+                    None,
+                ),
+                BasicBlockData::with(
+                    vec![
+                        Instruction::Write(Value(0)),
+                        Instruction::Branch(BranchOpcode::Br, BasicBlock(3)),
+                    ],
+                    None,
+                    Some(BasicBlock(3)),
+                    None,
+                ),
+                BasicBlockData::with(
+                    vec![
+                        Instruction::Write(Value(1)),
+                    ],
+                    Some(BasicBlock(3)),
+                    None,
+                    None,
+                ),
+                BasicBlockData::with(
+                    vec![
+                        Instruction::StoredBinaryOp {
+                            opcode: StoredBinaryOpcode::Add,
+                            src1: Value(0),
+                            src2: Value(1),
+                            dest: Value(2),
+                        },
+                        Instruction::Write(Value(2)),
+                        Instruction::Writeln,
+                        Instruction::End,
+                    ],
+                    None,
+                    None,
+                    None,
+                ),
+            ],
+            Some(BasicBlock(0)),
+        );
+
+        let ig = InterferenceGraph::from(&body);
+        assert_eq!(ig, InterferenceGraph(hashmap! {
+            Value(0) => hashset! { Value(1) },
+            Value(1) => hashset! { Value(0) },
+            Value(2) => HashSet::new(),
+        }));
+    }
+
+    #[test]
+    fn construct_ig_from_loop() {
         /*
             $0 = read
             $1 = read
