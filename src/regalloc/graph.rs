440bebc7d8e618c9563052634cd8ee753480f2c5
Arthur Lafrance <lafrancearthur@gmail.com>
2022-04-25 20:17:33 -0700

    impl ig construction for loops

--- src/regalloc/graph.rs
+++ src/regalloc/graph.rs
@@ -38,14 +38,14 @@ impl InterferenceGraph {
 
         let bb_data = body.basic_block_data(bb);
 
-        match bb_data.control_flow_kind() {
+        match bb_data.control_flow_kind(body) {
             ControlFlowKind::Leaf => self.construct_from_basic_block(bb_data, live_set),
             ControlFlowKind::FallthroughOnly(dest) | ControlFlowKind::UnconditionalBranch(dest) => {
                 self.visit_basic_block(body, dest, live_set, target);
                 self.construct_from_basic_block(bb_data, live_set);
             },
             ControlFlowKind::IfStmt(then_bb, else_bb) => self.visit_if_stmt(body, bb_data, then_bb, else_bb, live_set, target),
-            // ControlFlowKind::Loop(header, body, follow) => {},
+            ControlFlowKind::Loop(header_bb, body_bb) => self.visit_loop(body, bb_data, header_bb, body_bb, live_set, target),
         }
     }
 
@@ -80,6 +80,61 @@ impl InterferenceGraph {
         }
 
         self.construct_from_basic_block(condition_bb_data, live_set);
+    }
+
+    fn visit_loop(
+        &mut self,
+        body: &Body,
+        prev_bb_data: &BasicBlockData,
+        header_bb: BasicBlock,
+        body_bb: BasicBlock,
+        live_set: &mut HashSet<Value>,
+        target: Option<BasicBlock>,
+    ) {
+        // visit follow bb
+        let header_bb_data = body.basic_block_data(header_bb);
+        let follow_bb = header_bb_data.branch_dest().unwrap();
+        self.visit_basic_block(body, follow_bb, live_set, target);
+
+        // use live set, construct from header bb
+        let original_live_set = live_set.clone();
+        self.construct_from_basic_block(header_bb_data, live_set);
+
+        // config live set for body
+            // remove left side of phis
+        let mut body_live_set = live_set.clone();
+
+        for (prev_val, _) in header_bb_data.phis() {
+            body_live_set.remove(&prev_val);
+        }
+
+        // visit body bb (target = header bb)
+        self.visit_basic_block(body, body_bb, &mut body_live_set, Some(header_bb));
+
+        // combine body live set with original live set
+            // double check this
+
+        let diff: HashSet<_> = live_set.difference(&original_live_set).copied().collect();
+
+        for val in diff.into_iter() {
+            live_set.remove(&val);
+        }
+
+        for val in body_live_set.into_iter() {
+            live_set.insert(val);
+        }
+
+        // construct from header bb again
+        self.construct_from_basic_block(header_bb_data, live_set);
+
+        // config live set for prev bb
+            // remove right side of phis
+        for (_, body_val) in header_bb_data.phis() {
+            live_set.remove(&body_val);
+        }
+
+        // construct from prev bb
+        self.construct_from_basic_block(prev_bb_data, live_set);
     }
 }
 
@@ -186,7 +241,6 @@ mod tests {
     }
 
     #[test]
-    #[ignore]
     fn construct_ig_from_if_stmt() {
         // outputs the max of two input values
         /*
@@ -271,40 +325,88 @@ mod tests {
     #[test]
     fn construct_ig_from_loop() {
         /*
-            $0 = read
-            $1 = read
-            write $0
+            BB0:
+                $0 = const 0
+                $1 = const 1
+                $2 = read
 
-            $2 = add $0, $1
-            write $2
+            BB1:
+                $4 = phi $0, $3
+                cmp $4, $2
+                bge BB3
+
+            BB2:
+                $3 = add $4, $1
+                br BB1
+
+            BB3:
+                write $4
+                writeln
+                end
         */
 
         let body = Body::from(
-            vec![BasicBlockData::with(
-                vec![
-                    Instruction::Read(Value(0)),
-                    Instruction::Read(Value(1)),
-                    Instruction::Write(Value(0)),
-                    Instruction::StoredBinaryOp {
-                        opcode: StoredBinaryOpcode::Add,
-                        src1: Value(0),
-                        src2: Value(1),
-                        dest: Value(2),
-                    },
-                    Instruction::Write(Value(2)),
-                ],
-                None,
-                None,
-                None,
-            )],
+            vec![
+                BasicBlockData::with(
+                    vec![
+                        Instruction::Const(0, Value(0)),
+                        Instruction::Const(1, Value(1)),
+                        Instruction::Read(Value(2)),
+                    ],
+                    Some(BasicBlock(1)),
+                    None,
+                    None,
+                ),
+                BasicBlockData::with(
+                    vec![
+                        Instruction::StoredBinaryOp {
+                            opcode: StoredBinaryOpcode::Phi,
+                            src1: Value(0),
+                            src2: Value(3),
+                            dest: Value(4),
+                        },
+                        Instruction::Cmp(Value(4), Value(2)),
+                        Instruction::Branch(BranchOpcode::Bge, BasicBlock(3)),
+                    ],
+                    Some(BasicBlock(2)),
+                    Some(BasicBlock(3)),
+                    None,
+                ),
+                BasicBlockData::with(
+                    vec![
+                        Instruction::StoredBinaryOp {
+                            opcode: StoredBinaryOpcode::Add,
+                            src1: Value(4),
+                            src2: Value(1),
+                            dest: Value(3),
+                        },
+                        Instruction::Branch(BranchOpcode::Br, BasicBlock(1)),
+                    ],
+                    None,
+                    Some(BasicBlock(1)),
+                    None,
+                ),
+                BasicBlockData::with(
+                    vec![
+                        Instruction::Write(Value(4)),
+                        Instruction::Writeln,
+                        Instruction::End,
+                    ],
+                    None,
+                    None,
+                    None,
+                ),
+            ],
             Some(BasicBlock(0)),
         );
 
         let ig = InterferenceGraph::from(&body);
         assert_eq!(ig, InterferenceGraph(hashmap! {
-            Value(0) => hashset! { Value(1) },
-            Value(1) => hashset! { Value(0) },
-            Value(2) => HashSet::new(),
+            Value(0) => hashset! { Value(1), Value(2) },
+            Value(1) => hashset! { Value(0), Value(2), Value(4) },
+            Value(2) => hashset! { Value(0), Value(1), Value(3), Value(4) },
+            Value(3) => hashset! { Value(2) },
+            Value(4) => hashset! { Value(1), Value(2) },
         }));
     }
 }
