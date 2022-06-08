39ddeaadc626965341d62d126dbe9d00b863fa49
Arthur Lafrance <lafrancearthur@gmail.com>
2022-06-07 21:01:34 -0700

    more e2e tests (well, one more)

--- tinyc/tests/e2e.rs
+++ tinyc/tests/e2e.rs
@@ -26,6 +26,7 @@ mod e2e {
     define_e2e_test!(if_then_if);
     define_e2e_test!(if_then_loop);
     define_e2e_test!(if_with_else_with_phis);
+    define_e2e_test!(instr_select);
     define_e2e_test!(loop_no_phis);
     define_e2e_test!(loop_with_if);
     define_e2e_test!(loop_with_loop);
